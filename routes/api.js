6561f1c114a7395432a672a9d1d71e7bd383ac4d
Renan Pimentel <renan.pimentel@gmail.com>
2015-02-03 14:11:57 -0200

    general update

--- routes/api.js
+++ routes/api.js
@@ -1,22 +1,50 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+
+router.use(function (req, res, next) {
+  if(req.method === 'POST' || req.method === 'PUT') {
+    if (!req.is('application/json')) {
+      res.status(415);
+      res.json({
+        'error': 'content-type error',
+        'message': 'Use content-type: application/json'
+      });
+      return;
+    }
+  }
+  next();
+});
+
 router.get('/get', function(req, res, next) {
+  res.status(200);
   res.send('response GET');
 });
 
+router.get('/get/param/:id', function(req, res, next) {
+  res.status(200);
+  var data = req.params;
+  res.send('response GET ' + data.id);
+});
+
+
 router.post('/post', function(req, res, next) {
   var data = req.body;
+  res.status(200);
   res.json({'method': 'POST', 'response': data});
 });
 
 router.put('/put', function(req, res, next) {
-  res.send('respond with a resource');
+  var data = req.body;
+  res.status(201);
+  res.json({'method': 'PUT', 'response': data});
 });
 
-router.delete('/delete', function(req, res, next) {
-  res.send('respond with a resource');
+router.delete('/delete/:id', function(req, res, next) {
+  var data = req.params;
+  res.status(410);
+  res.send('response DELETE' + data.id);
 });
 
 module.exports = router;
+
